eb91a944447e7f2da03179a0d481f2654647decf
Nurul <nurulislamz2600@gmail.com>
2024-08-04 12:45:29 +0100

    added connect4 game

---
+++ Connect4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Connect4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Game game = new Game(6, 7, 4, 1);
+            game.Play();
+        }
+    }
+}
