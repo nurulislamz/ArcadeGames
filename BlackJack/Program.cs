9419c7870945ca75c925c26a1119306e2f94e985
Nurul <nurulislamz2600@gmail.com>
2024-08-05 00:41:25 +0100

    BlackJack Game

---
+++ BlackJack/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+public enum CardNumber
+{
+    Ace = 1,
+    Two = 2,
+    Three = 3,
+    Fou = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Jack = 10,
+    Queen = 10,
+    King = 10
+}
+
+public enum CardSuits
+{
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+}
+
+public class Card
+{
+
+    CardNumber cardNumber { get; set; }
+    CardSuits cardSuit { get; set; }
+
+    public Card(CardSuits cardSuits, CardNumber cardNumber)
+    {
+        this.cardSuit = cardSuits;
+        this.cardNumber = cardNumber;
+    }
+    
+    public string PrintCards()
+    {
+        return $"{this.cardSuit}: {this.cardNumber}";
+    }
+
+    public static int operator +(int a, Card b)
+    {
+        if (b.cardNumber == CardNumber.Ace)
+        {
+            int result1 = a + 11;
+            int result2 = a + 1;
+
+            if (result1 <= 21) return result1;
+            else return result2;
+        }
+        
+        return a + (int) b.cardNumber;
+    }
+}
+
+public class Deck
+{
+    public List<Card> deck { get; set; }
+
+    public Deck()
+    {
+        foreach (CardSuits suits in Enum.GetValues(typeof(CardSuits)))
+        {
+            foreach (CardNumber number in Enum.GetValues(typeof(CardNumber)))
+            {
+                deck.Add(new Card(suits, number));
+            }
+        }
+    }
+
+    public Card GetRandomCardFromDeck()
+    {
+        Random random = new Random();
+        CardSuits suits = (CardSuits) random.Next(0, 3);
+        CardNumber numbers = (CardNumber) random.Next(0, 11);
+
+        Card card = new Card(suits, numbers);
+
+        if (RemoveCardFromDeck(card)) return card;
+        else
+        {
+            throw new Exception("Card already in deck, retrying");
+        }
+    }
+
+    public bool RemoveCardFromDeck(Card card)
+    {
+        if (!deck.Contains(card))
+        {
+            deck.Remove(card);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ BlackJack/readme.txt
@@ -0,0 +1,30 @@
+1) Requirements
+- Cards 
+-- Jack, Queen and Kings are valued at 10.
+-- Ace is valued at 1 or 11 (whatever is closest to 21)
+
+- Aim of the game is to get closest to 21.
+
+- If you get 21, you win (thats a blackjack! )
+
+- Players need to have a amount to bet
+- AI needs to have a amount to bet
+- Both need a deposit
+
+- Score tracking system
+
+2) Design - High Level
+Cards Class
+- Value Enum
+- Suit record
+
+- deck class
+- <suits, card number> deck of cards
+
+- Game class
+
+- Player class
+
+
+
+
